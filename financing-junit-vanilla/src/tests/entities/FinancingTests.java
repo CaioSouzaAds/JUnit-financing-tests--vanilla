01bea0b1db3d3131f1d5a6a86a4ab7ab60bb7623
CaioSouzaAds <caio.cesarads@gmail.com>
2023-11-11 15:58:13 -0300

    Introduce unit tests for invalid data in constructor, catch exception when creating an object with invalid data

--- financing-junit-vanilla/src/tests/entities/FinancingTests.java
+++ financing-junit-vanilla/src/tests/entities/FinancingTests.java
@@ -7,23 +7,37 @@ import entities.Financing;
 import tests.factory.FinancingFactory;
 
 public class FinancingTests {
-	
+
 	@Test
 	public void constructorShouldCreateObjectWhenValidData() {
-		
+
 		// Arrange
 		double totalAmountExpected = 100000.0;
 		double incomeExpected = 2000.0;
 		Integer monthsExpected = 80;
-		
+
 		// Action
-		Financing fnc = FinancingFactory.createFinancing(
-				totalAmountExpected, incomeExpected, monthsExpected);
-		
+		Financing fnc = FinancingFactory.createFinancing(totalAmountExpected, incomeExpected, monthsExpected);
+
 		// Assert
 		Assertions.assertEquals(totalAmountExpected, fnc.getTotalAmount());
 		Assertions.assertEquals(incomeExpected, fnc.getIncome());
 		Assertions.assertEquals(monthsExpected, fnc.getMonths());
+	}
+
+	@Test
+	public void constructorShouldThrowExceptionWhenCreateObjectIsInvalidData() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			// Arrange
+			double totalAmountExpected = 100000.0;
+			double incomeExpected = 2000.0;
+			Integer monthsExpected = 20;
+
+			// Action
+			Financing fnc = FinancingFactory.createFinancing(totalAmountExpected, incomeExpected, monthsExpected);
+		});
+
 	}
 
 }
