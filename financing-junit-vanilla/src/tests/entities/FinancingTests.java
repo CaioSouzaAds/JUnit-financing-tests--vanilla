28b4e2e7bc9068fc7a8afd10a20f9514d6adf043
CaioSouzaAds <caio.cesarads@gmail.com>
2023-11-11 17:17:28 -0300

    Added tests for setTotalAmount: update with valid data and throw exception with invalid data.

--- financing-junit-vanilla/src/tests/entities/FinancingTests.java
+++ financing-junit-vanilla/src/tests/entities/FinancingTests.java
@@ -40,4 +40,42 @@ public class FinancingTests {
 
 	}
 
+	@Test
+	public void setTotalAmountShouldUpdateTotalAmountWhenValidData() {
+		// Arrange
+		double totalAmountExpected = 50000.0;
+		double updatedTotalAmount = 100000.0;
+		double incomeExpected = 2000.0;
+		Integer monthsExpected = 80;
+
+		Financing fnc = FinancingFactory.createFinancing(totalAmountExpected, incomeExpected, monthsExpected);
+
+		// Action
+		fnc.setTotalAmount(updatedTotalAmount);
+
+		// Assert
+		Assertions.assertEquals(updatedTotalAmount, fnc.getTotalAmount());
+	}
+
+	@Test
+	public void setTotalAmountShouldUpdateTotalAmountWhenIsInvalidData() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+
+			// Arrange
+
+			double totalAmountExpected = 100000.0;
+			double updatedTotalAmount = 110000.0;
+			double incomeExpected = 2000.0;
+			Integer monthsExpected = 80;
+
+			Financing fnc = FinancingFactory.createFinancing(totalAmountExpected, incomeExpected, monthsExpected);
+
+			// Action
+			fnc.setTotalAmount(updatedTotalAmount);
+
+		});
+
+	}
+
 }
